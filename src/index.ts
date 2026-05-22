8bf66cefafc2baa56f47880b5da0e39001d3f4c6
phurpa-tsering <phurpa@apyhub.com>
2026-05-22 17:04:09 +0530

    feat: initial plugin source

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Postman Collection Importer Extension
+ *
+ * Enables importing Postman collections (v2.1) and converting them to Voiden .void request files.
+ */
+
+export { default } from './main';
+export { PostmanImportButton } from './components/PostmanImportButton';
+export * from './utils/types';
+export * from './utils/converter';
